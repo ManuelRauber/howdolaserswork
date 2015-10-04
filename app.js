bd124a7f2a15e30c5fa262e0bbda52f8d456c543
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-10-04 23:22:53 +0200

    Pew Pew Animation!

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 !function () {
     'use strict';
 
-    function playRandomPewPewSounds(audioElement) {
+    var lastFlashIndex = 2;
+
+    function flashPewPew(pewpew) {
+        pewpew.classList.add('flash-pew-pew');
+
+        setTimeout(function () {
+            pewpew.classList.remove('flash-pew-pew');
+        }, 250);
+    }
+
+    function flashPewPews(pewpew1, pewpew2) {
+        if (lastFlashIndex === 2) {
+            flashPewPew(pewpew1);
+            lastFlashIndex = 1;
+            return;
+        }
+
+        flashPewPew(pewpew2);
+        lastFlashIndex = 2;
+    }
+
+    function playRandomPewPewSounds(audioElement, pewpew1, pewpew2) {
         var nextSound = Math.random() * 1500;
 
         setTimeout(function () {
             audioElement.play();
+            flashPewPews(pewpew1, pewpew2);
         }, nextSound);
     }
 
     document.addEventListener('DOMContentLoaded', function () {
         var htmlAudioElement = document.getElementById('pewpewSound');
+        var pewpew1 = document.getElementById('pewpew-1');
+        var pewpew2 = document.getElementById('pewpew-2');
 
         htmlAudioElement.addEventListener('ended', function () {
-            playRandomPewPewSounds(htmlAudioElement);
+            playRandomPewPewSounds(htmlAudioElement, pewpew1, pewpew2);
         });
 
-        playRandomPewPewSounds(htmlAudioElement);
+        playRandomPewPewSounds(htmlAudioElement, pewpew1, pewpew2);
     });
 }();
